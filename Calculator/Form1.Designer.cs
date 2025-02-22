68de4cc942d303b54d1b81f77be1bc833f0e266c
minh phung <butdeptrai2k@gmail.com>
2025-02-22 15:47:41 +0700

    Xu ly nut Cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -31,9 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.Cong = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,7 +46,7 @@
             this.label1.Size = new System.Drawing.Size(39, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Số a";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+     
             // 
             // label2
             // 
@@ -56,7 +57,7 @@
             this.label2.Size = new System.Drawing.Size(39, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Số b";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+         
             // 
             // label3
             // 
@@ -68,38 +69,49 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Kết Quả";
             // 
-            // textBox1
+            // txtA
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(91, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(202, 24);
-            this.textBox1.TabIndex = 3;
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.Location = new System.Drawing.Point(91, 38);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(202, 24);
+            this.txtA.TabIndex = 3;
             // 
-            // textBox2
+            // txtB
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(91, 78);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(202, 24);
-            this.textBox2.TabIndex = 4;
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.Location = new System.Drawing.Point(91, 78);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(202, 24);
+            this.txtB.TabIndex = 4;
             // 
-            // textBox3
+            // txtKetQua
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(91, 116);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(202, 24);
-            this.textBox3.TabIndex = 5;
+            this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(91, 116);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(202, 24);
+            this.txtKetQua.TabIndex = 5;
+            // 
+            // Cong
+            // 
+            this.Cong.Location = new System.Drawing.Point(50, 168);
+            this.Cong.Name = "Cong";
+            this.Cong.Size = new System.Drawing.Size(75, 23);
+            this.Cong.TabIndex = 6;
+            this.Cong.Text = "Cộng";
+            this.Cong.UseVisualStyleBackColor = true;
+            this.Cong.Click += new System.EventHandler(this.Cong_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(344, 261);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Cong);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -115,9 +127,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Button Cong;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -17,14 +17,13 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+       
+        private void Cong_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
         }
     }
 }
